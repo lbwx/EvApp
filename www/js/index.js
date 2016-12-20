81a5b819315e64592064c0b4c07052eebfd3db7b
lbwx <lb@webaholix.com>
2016-12-20 16:01:34 +0100

    qr scanner now works in compiled app!

--- www/js/index.js
+++ www/js/index.js
@@ -547,20 +547,16 @@ var eventWindow = {
 	},
 	scan: function() {
 		cordova.plugins.barcodeScanner.scan(
-		  function (result) {
-			if(!result.cancelled)
-			{
-			  alert("Barcode type is: " + result.format);
-			  alert("Decoded text is: " + result.text);
+			function (result) {
+				if(!result.cancelled) {
+					alert("Decoded text is: " + result.text);
+				} else {
+					alert("You have cancelled scan");
+				}
+			},
+			function (error) {
+				alert("Scanning failed: " + error);
 			}
-			else
-			{
-			  alert("You have cancelled scan");
-			}
-		  },
-		  function (error) {
-			  alert("Scanning failed: " + error);
-		  }
 		);
 	}
 };
