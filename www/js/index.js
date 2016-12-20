cf5020c837d278935d5e399b2fe16ae688c0877d
lbwx <lb@webaholix.com>
2016-12-20 14:24:45 +0100

    test of qr scanner v1.01

--- www/js/index.js
+++ www/js/index.js
@@ -542,15 +542,30 @@ var eventWindow = {
 	},
 	scan: function() {
 		cordova.plugins.barcodeScanner.scan(
-				function (result) {
-					if(!result.cancelled) {
-						alert("Barcode type is: " + result.format);
-						alert("decoded text is: " + result.text);
-					} else {
-						alert("You have canceled scan");
+			function (result) {
+				if(!result.cancelled)
+				{
+					if(result.format == "QR_CODE")
+					{
+						navigator.notification.prompt("Please enter name of data",  function(input){
+							var name = input.input1;
+							var value = result.text;
+
+							var data = localStorage.getItem("LocalData");
+							console.log(data);
+							data = JSON.parse(data);
+							data[data.length] = [name, value];
+
+							localStorage.setItem("LocalData", JSON.stringify(data));
+
+							alert("Done");
+						});
 					}
-				},
-				function (error) { alert('Scanning failed: ' + error); }
-		);
+				}
+			},
+			function (error) {
+				alert("Scanning failed: " + error);
+			}
+	   );
 	}
 };
